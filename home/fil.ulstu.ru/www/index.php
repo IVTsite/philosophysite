fe0867788054309ddb5d2e235c1a6fad0b3b1145
Dashulay <d.menshova@list.ru>
2015-11-05 15:41:45 +0400

    no message

--- home/fil.ulstu.ru/www/index.php
+++ home/fil.ulstu.ru/www/index.php
@@ -1,4 +1,13 @@
+<!DOCTYPE html>
+
 <html>
+<head>
+	<title>Philosofy site</title>
+	<meta charset="windows-1251">
+	<link href="css/main.css" rel = "stylesheet" type="text/css">
+</head>
+<body>
+</body>
 </html>
 
 
